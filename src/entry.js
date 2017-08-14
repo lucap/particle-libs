3f9ef40af53839a38e5a39d76eef7b0cc1bfa496
Luca Pellicoro <luca@hipmunk.com>
2017-08-13 21:26:22 -0700

    get react working

---
+++ src/entry.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const main = () => {
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('root')
+    );
+}
+
+main();
