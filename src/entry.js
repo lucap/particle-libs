67c7f62cf060b39f12270fb6b8bf8b5adf76d27c
Luca Pellicoro <luca@hipmunk.com>
2017-08-21 23:45:16 -0700

    start work on detail page

--- src/entry.js
+++ src/entry.js
@@ -1,11 +1,29 @@
 import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import LibraryList from './LibraryList';
+
+const AUTH_TOKEN = '930a28831457f828330548d8f83a5b9f10d0bebf';
+
 
 const main = () => {
+    // poor man's routing
+    // read the library name from the url
+    // we use the fragment for simplicity
+    const page = window.location.hash.split('#')[1]
+        ? (
+            <LibraryList
+                authToken={AUTH_TOKEN}
+            />
+        )
+        : (
+            <LibraryList
+                authToken={AUTH_TOKEN}
+            />
+        );
+
     ReactDOM.render(
-        <App/>,
+        page,
         document.getElementById('root')
     );
 }
